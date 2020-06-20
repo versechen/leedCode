3a7f2b60726f2d755e8686bbea4c73fb348ec4c4
WeiCSir <49602537+WeiCSir@users.noreply.github.com>
2020-06-20 22:41:09 +0800

    gitignore add

--- 寻找最长回文子串/main_normal.cpp
+++ 寻找最长回文子串/main_normal.cpp
@@ -1,51 +1,14 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<thread>
 using namespace std;
 using Vec = vector<int>;
 
-string findLongSubstring(string& str)
-{
-	int start = 0, len = 0;
-
-	for (int i = 0; i < str.size(); i++)
-	{
-		int temp1 = i;
-		int temp2 = i + 1;
-		while (temp1 >= 0 and temp2 < str.size() and str[temp1] == str[temp2])
-		{
-			temp1--;
-			temp2++;
-		}
-
-		if (len < ((temp2 - i) * 2))
-		{
-			start = temp1 + 1;
-			len = (temp2 - 1 - i) * 2;
-		}
-		temp1 = i;
-		temp2 = i + 2;
-		while (temp1 >= 0 and temp2 < str.size() and str[temp1] == str[temp2])
-		{
-			temp1--;
-			temp2++;
-		}
-
-		if (len < ((temp2 - 1 - i) * 2 + 1))
-		{
-			start = temp1 + 1;
-			len = (temp2 - i) * 2 + 1;
-		}
-
-	}
-
-	return str.substr(start, len);
-}
-
 int main()
 {
-	string a("ggabbahh");
-	cout << findLongSubstring(a) << endl;
+	const int limits = thread::hardware_concurrency();
+	cout << limits << endl;
 }
 
 
