2d800d36ce1657b885455a4bdff494c539b15dac
weicsir <weicsir@outlook.com>
2023-07-13 21:25:49 +0800

    feature: add 两数之和

---
+++ 两数之和/main.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
